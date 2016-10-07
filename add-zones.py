baea5ef5bb2dc043dd522770975cf5e6c53f7f62
Sean Collins <sean@cllns.com>
2016-10-07 12:52:57 -0600

    Start generalizing, and improve output

--- add-zones.py
+++ add-zones.py
@@ -2,7 +2,7 @@ import pandas as pd
 import requests
 import json
 
-QUERY = 'https://gisweb.bouldercolorado.gov/arcgis/rest/services/pds/AddressSearch/MapServer/1/query?where=ASR_ID+%3D+{}&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson'
+QUERY = 'https://gisweb.bouldercolorado.gov/arcgis/rest/services/pds/{}/MapServer/1/query?where=ASR_ID+%3D+{}&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson'
 
 file_name = 'ResidentialBuildingSizes20161003.xlsx'
 
@@ -10,17 +10,17 @@ class ZoningInfo:
   def __init__(self):
     self.df = pd.read_excel("input/{}".format(file_name), converters={'ASR_ID': str})
     self.ids = self.df['ASR_ID']
+    self.url_name = "AddressSearch"
     self.zoning = {}
     self.address = {}
     self.lot_size = {}
     self.neighborhood = {}
 
   def update(self):
-    no_features = 0
+    error_ids = []
 
     for i in self.ids[:20]:
-        print "Getting info for {}".format(i)
-        r = requests.get(QUERY.format(i))
+        r = requests.get(QUERY.format(self.url_name, i))
         j = json.loads(r.text)
         try:
             attributes = j['features'][0]['attributes']
@@ -28,18 +28,23 @@ class ZoningInfo:
             self.address[i] =      attributes['ADDRESS']
             self.lot_size[i] =     attributes['AREASQFT']
             self.neighborhood[i] = attributes['NEIGHBRHD']
-        except IndexError:
-            print "No features for {}".format(i)
-            no_features += 1
-        print "Done!\n"
+            print "{:20} -> {}".format(attributes['ADDRESS'], attributes['ZONING'])
+        except (KeyError, IndexError):
+            error_ids.append(i)
+    print "\nError ASR_ID's: {}\n".format(error_ids)
+    print "Done getting all zones!"
 
   def write(self):
+    out_file_name = 'output/output.xlsx'
+    print "Writing them out to {}...".format(out_file_name)
     self.df['zoning'] =       self.df['ASR_ID'].map(self.zoning)
     self.df['address'] =      self.df['ASR_ID'].map(self.address)
     self.df['lot_size'] =     self.df['ASR_ID'].map(self.lot_size)
     self.df['neighborhood'] = self.df['ASR_ID'].map(self.neighborhood)
 
-    self.df.to_excel('output/with_zones.xlsx', index=False)
+    self.df.to_excel(out_file_name, index=False)
+    print "Done writing to {}!".format(out_file_name)
+
 
 zoning_info = ZoningInfo()
 zoning_info.update()
