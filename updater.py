5aa68def693ab2797349a37d2f0e75b2a917eb17
Sean Collins <sean@cllns.com>
2016-10-07 16:22:49 -0600

    Chunk requests to drastically improve performance (though we cannot show rental info in STDOUT now

--- updater.py
+++ updater.py
@@ -3,9 +3,13 @@ import requests
 import json
 from datetime import datetime
 
-QUERY = 'https://gisweb.bouldercolorado.gov/arcgis/rest/services/pds/{}/MapServer/1/query?where=ASR_ID+%3D+{}&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson'
+QUERY = 'https://gisweb.bouldercolorado.gov/arcgis/rest/services/pds/{}/MapServer/1/query?where=ASR_ID+IN+({})&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=false&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson'
 
 file_name = 'ResidentialBuildingSizes20161003.xlsx'
+
+def chunks(l, n):
+  n = max(1, n)
+  return (l[i:i+n] for i in xrange(0, len(l), n))
 
 class ZoningInfo:
   def __init__(self):
@@ -26,30 +30,39 @@ class ZoningInfo:
     #     for index, asr_id in enumerate(self.ids[:20]):
     # And this to process all the data (it will take a long time)
     #     for index, asr_id in enumerate(self.ids[:20]):
-    for index, asr_id in enumerate(self.ids[:20]):
-      try:
-        r = requests.get(QUERY.format("AddressSearch", asr_id))
-        attributes = json.loads(r.text)['features'][0]['attributes']
-        self.zoning[asr_id] =       attributes['ZONING']
-        self.address[asr_id] =      attributes['ADDRESS']
-        self.lot_size[asr_id] =     attributes['AREASQFT']
-        self.neighborhood[asr_id] = attributes['NEIGHBRHD']
-      except (KeyError, IndexError):
-        error_ids.append(asr_id)
-      else:
+
+    chunk_size = 199
+
+    chunkz = chunks(self.ids, chunk_size)
+
+    for chunk_number, chunk in enumerate(chunkz):
+      r = requests.get(QUERY.format("AddressSearch", ",".join(chunk)))
+      for record_number, record in enumerate(json.loads(r.text)['features']):
         try:
-          r = requests.get(QUERY.format("RentalInquiry", asr_id))
-          attributes = json.loads(r.text)['features'][0]['attributes']
+          attributes = record['attributes']
+          asr_id = attributes['ASR_ID']
+          self.zoning[asr_id] =       attributes['ZONING']
+          self.address[asr_id] =      attributes['ADDRESS']
+          self.lot_size[asr_id] =     attributes['AREASQFT']
+          self.neighborhood[asr_id] = attributes['NEIGHBRHD']
+        except (KeyError, IndexError):
+          error_ids.append(asr_id)
+        else:
+          print "{:.6f}% {:10} {:30} {:10} {}".format(
+            ((chunk_size * chunk_number) + record_number) / float(number_of_ids),
+            asr_id,
+            self.address[asr_id],
+            self.zoning[asr_id],
+            "RENTAL" if asr_id in self.rental_license_number else ""
+          )
+
+      r = requests.get(QUERY.format("RentalInquiry", ",".join(chunk)))
+      for record_number, record in enumerate(json.loads(r.text)['features']):
+        try:
+          attributes = record['attribues']
           self.rental_license_number[asr_id] = attributes['PROP_NO']
         except (KeyError, IndexError):
           None
-        print "{:.6f}% {:10} {:30} {:10} {}".format(
-          index / float(number_of_ids),
-          asr_id,
-          self.address[asr_id],
-          self.zoning[asr_id],
-          "RENTAL" if asr_id in self.rental_license_number else ""
-        )
     print "\nError ASR_ID's: {}\n".format(error_ids)
     print "Done getting all zones!"
 
