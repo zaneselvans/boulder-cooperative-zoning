46f7dfe566125a6d2eee01943f60e61fd7c821c2
Sean Collins <sean@cllns.com>
2016-10-06 21:30:00 -0600

    Class-ify script a little

--- add-zones.py
+++ add-zones.py
@@ -4,34 +4,42 @@ import json
 
 QUERY = 'https://gisweb.bouldercolorado.gov/arcgis/rest/services/pds/AddressSearch/MapServer/1/query?where=ASR_ID+%3D+{}&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson'
 
-df = pd.read_excel('input/ResidentialBuildingSizes20161003.xlsx', converters={'ASR_ID': str})
-ids = df['ASR_ID']
 
-zoning = {}
-address = {}
-lot_size = {}
-neighborhood = {}
-no_features = 0
-for i in ids[:20]:
-    print "Getting info for {}".format(i)
-    r = requests.get(QUERY.format(i))
-    j = json.loads(r.text)
-    try:
-        attributes = j['features'][0]['attributes']
-        zoning[i] = attributes['ZONING']
-        address[i] = attributes['ADDRESS']
-        lot_size[i] = attributes['AREASQFT']
-        neighborhood[i] = attributes['NEIGHBRHD']
-    except:
-        print "No features"
-        no_features += 1
-    print "Done"
+class ZoningInfo:
+  def __init__(self):
+    self.df = pd.read_excel('input/ResidentialBuildingSizes20161003.xlsx', converters={'ASR_ID': str})
+    self.ids = self.df['ASR_ID']
+    self.zoning = {}
+    self.address = {}
+    self.lot_size = {}
+    self.neighborhood = {}
 
-print "{} with no features".format(no_features)
+  def update(self):
+    no_features = 0
 
-df['zoning'] = df['ASR_ID'].map(zoning)
-df['address'] = df['ASR_ID'].map(address)
-df['lot_size'] = df['ASR_ID'].map(lot_size)
-df['neighborhood'] = df['ASR_ID'].map(neighborhood)
+    for i in self.ids[:20]:
+        print "Getting info for {}".format(i)
+        r = requests.get(QUERY.format(i))
+        j = json.loads(r.text)
+        try:
+            attributes = j['features'][0]['attributes']
+            self.zoning[i] = attributes['ZONING']
+            self.address[i] = attributes['ADDRESS']
+            self.lot_size[i] = attributes['AREASQFT']
+            self.neighborhood[i] = attributes['NEIGHBRHD']
+        except IndexError:
+            print "No features for {}".format(i)
+            no_features += 1
+        print "Done!\n"
 
-df.to_excel('output/MASTER.xlsx', index=False)
+  def write(self):
+    self.df['zoning'] =       self.df['ASR_ID'].map(self.zoning)
+    self.df['address'] =      self.df['ASR_ID'].map(self.address)
+    self.df['lot_size'] =     self.df['ASR_ID'].map(self.lot_size)
+    self.df['neighborhood'] = self.df['ASR_ID'].map(self.neighborhood)
+
+    self.df.to_excel('output/with_zones.xlsx', index=False)
+
+zoning_info = ZoningInfo()
+zoning_info.update()
+zoning_info.write()
