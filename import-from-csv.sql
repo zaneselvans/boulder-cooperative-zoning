d570e50e9465894b33c1f3cae4fad9a898727419
Sean Collins <sean@cllns.com>
2016-10-11 16:02:29 -0600

    add sqlite script for importing from CSV

---
+++ import-from-csv.sql
@@ -0,0 +1,26 @@
+DROP TABLE properties;
+CREATE TABLE properties(
+  "assessor_id" INTEGER,
+  "parcel_number" INTEGER,
+  "parcel_number_building_id" TEXT,
+  "building_year" YEAR,
+  "building_type" TEXT,
+  "finished_above_grade_sq_ft" INTEGER,
+  "unfinished_above_grade_sq_ft" INTEGER,
+  "finished_below_grade_sq_ft" INTEGER,
+  "unfinished_below_grade_sqft" INTEGER,
+  "car_storage_ft" INTEGER,
+  "other_sq_ft" INTEGER,
+  "outbuilding_sq_ft" INTEGER,
+  "total_finished_sq_ft" INTEGER,
+  "size_category" TEXT,
+  "zone" TEXT,
+  "address" TEXT,
+  "lot_size" INTEGER,
+  "neighborhood" TEXT,
+  "rental_license" TEXT
+);
+
+.mode csv
+.headers on
+.import output/output.csv properties
